29f9576a7585b0c4ff40af203e47e36bdc04672b
youssefhmk <youssefhesham2006K@gmail.com>
2026-07-16 07:48:08 +0300

    again

--- chef_claud/src/components/header.jsx
+++ chef_claud/src/components/header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <header className='flex justify-center items-center gap-4 height-80 box-shadow-md bg-white border-b border-gray-200'> 
-      <img src="../chef-claude-icon.png" alt="" className='w-16' />
+      <img src="/chef-claude-icon.png" alt="Chef Claude" className="w-16" />
       <h1 className='text-3xl font-bold'>Chef Claude</h1>
     </header>
   )
